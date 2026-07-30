079991eea259e0344c5d853407360fb0a687fae4
Chulseung Yoo <chulseungyoo@gmail.com>
2026-07-30 14:00:35 +0900

    Align VL6180X polling declarations for C23

--- ESP32_VL6180X/api/core/inc/vl6180x_platform.h
+++ ESP32_VL6180X/api/core/inc/vl6180x_platform.h
@@ -208,7 +208,7 @@ typedef struct MyDev_t *VL6180xDev_t;
  * @code
  * #define VL6180x_PollDelay(...) (void)0
  * @endcode
- * @param dev The device
+ * @param handle The device
  * @ingroup api_platform
  */
 void VL6180x_PollDelay(i2c_master_dev_handle_t* handle); /* usualy best implemanted a a real fucntion */
@@ -308,6 +308,5 @@ void VL6180x_PollDelay(i2c_master_dev_handle_t* handle); /* usualy best impleman
 #endif /* else */
 
 #endif  /* VL6180x_PLATFORM */
-
 
 
